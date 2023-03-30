690d62e5b9287fa6b0c473946f17d8040aac2308
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-03-30 17:09:53 -0400

    updated phone book with mock data testing filter function

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -3,7 +3,12 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arthur Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-123456", id: 1 },
+    { name: "Ada Lovelace", phone: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", phone: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", phone: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
