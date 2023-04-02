78d2bc25c5fb12e5c8555effa69faec7da76f12e
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-04-02 14:09:16 -0400

    added db section to state

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -1,22 +1,32 @@
 import React from "react";
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+//Componets
 import { Filter } from "./components/Filter";
 import { Persons } from "./components/Persons";
 import { PersonsForm } from "./components/PersonsForm";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-123456", id: 1 },
-    { name: "Ada Lovelace", phone: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", phone: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", phone: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
 
-  console.log(newNumber);
+  // const promise = axios.get("http://localhost:3001/persons");
+
+  // promise.then((response) => {
+  //   console.log(response);
+  // });
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "notes");
 
   const addPerson = (event) => {
     event.preventDefault();
