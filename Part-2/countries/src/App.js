4a3c1bb03b3854468d1431dcc8d61bff0655d548
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-04-10 15:50:27 -0400

    Added input section to country app

--- Part-2/countries/src/App.js
+++ Part-2/countries/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  const [country, setCountry] = useState("");
+
+  const handleCountry = (event) => {
+    console.log(event.target.value);
+    setCountry(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Country Search</h2>
+      <span>Find Countries...</span>
+      <input value={country} onChange={handleCountry} />
     </div>
   );
 }
