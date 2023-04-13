7343b00a91c5d7a1225f9906d570b271ed4f1fcd
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-04-13 19:26:53 -0400

    updated more of app 3

--- Part-3/index.js
+++ Part-3/index.js
@@ -25,8 +25,12 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(id);
+  const note = notes.find((note) => note.id === id);
+  console.log(note);
+  response.json(note);
 });
 
 const PORT = 3001;
