007856575691b9fc0832429a7a4acfbe6dc1fbd5
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-04-13 19:46:58 -0400

    update main backend with post

--- Part-3/index.js
+++ Part-3/index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const http = require("http");
 
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -25,10 +27,30 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
 app.get("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   console.log(id);
   const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const note = request.body;
   console.log(note);
   response.json(note);
 });
